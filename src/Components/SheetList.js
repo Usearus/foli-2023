53bd833a1be350be0cb530c179eebc580dd0e680
Adam <adamdenais@gmail.com>
2023-02-25 19:49:10 -0600

    Implemented hide/show sidebar to sheet visibility

--- src/Components/SheetList.js
+++ src/Components/SheetList.js
@@ -1,11 +1,17 @@
 import Sheet from './Sheet';
+import { useContext } from 'react';
+import { AirtableContext } from '../context/AirtableContext';
 
 const SheetList = ({ sheets, className }) => {
+  const { currentSheets } = useContext(AirtableContext);
+
   return (
     <div className={className}>
-      {sheets.map((sheet) => {
-        return <Sheet key={sheet.id} {...sheet} />;
-      })}
+      {sheets
+        .filter((sheet) => !currentSheets.find((s) => s.id === sheet.id).hidden)
+        .map((sheet) => {
+          return <Sheet key={sheet.id} {...sheet} id={sheet.id} />;
+        })}
     </div>
   );
 };
