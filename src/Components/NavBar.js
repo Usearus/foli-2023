7ff90d28ef5f676d272d5d6ef340ec6627a0a7d7
Adam <adamdenais@gmail.com>
2023-03-11 16:52:37 -0600

    Added Admin-only page

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,88 +1,90 @@
+import { useContext, useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import { Nav, Navbar, Button, Badge } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useAuth0 } from '@auth0/auth0-react';
 import ModalProfile from './ModalProfile';
+import { AirtableContext } from '../context/AirtableContext';
 
 const NavBar = () => {
   const { logout } = useAuth0();
+  const [admin, setAdmin] = useState(false);
+  const { userProfile } = useContext(AirtableContext);
 
-  return (
-    <Navbar
-      collapseOnSelect
-      expand='lg'
-      style={{
-        padding: '8px 16px',
-        background: 'var(--grey-100)',
-        borderBottom: '1px solid var(--grey-300)',
-        position: 'sticky',
-        top: '0',
-        zIndex: '1',
-      }}
-    >
-      <Container fluid>
-        <Navbar.Brand>
-          <span
-            style={{
-              fontWeight: 700,
-              fontSize: '1.5rem',
-              cursor: 'default',
-              paddingRight: '.25rem',
-            }}
-          >
-            fol<i>i</i>
-          </span>
-          <span>
-            <Badge
-              pill
-              bg='dark'
-              style={{ fontSize: '.6rem', marginRight: '1rem' }}
+  useEffect(() => {
+    if (userProfile?.fields?.account === 'adamdenais@gmail.com') {
+      setAdmin(true);
+    }
+  }, [userProfile]);
+
+  if (userProfile) {
+    return (
+      <Navbar
+        collapseOnSelect
+        expand='lg'
+        style={{
+          padding: '8px 16px',
+          background: 'var(--grey-100)',
+          borderBottom: '1px solid var(--grey-300)',
+          position: 'sticky',
+          top: '0',
+          zIndex: '1',
+        }}
+      >
+        <Container fluid>
+          <Navbar.Brand>
+            <span
+              style={{
+                fontWeight: 700,
+                fontSize: '1.5rem',
+                cursor: 'default',
+                paddingRight: '.25rem',
+              }}
             >
-              beta
-            </Badge>
-          </span>
-        </Navbar.Brand>
+              fol<i>i</i>
+            </span>
+            <span>
+              <Badge
+                pill
+                bg='dark'
+                style={{ fontSize: '.6rem', marginRight: '1rem' }}
+              >
+                beta
+              </Badge>
+            </span>
+          </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav variant='pills' className='me-auto'>
-            <LinkContainer to='/'>
-              <Nav.Link>Jobs</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/testing'>
-              <Nav.Link>Testing</Nav.Link>
-            </LinkContainer>
-          </Nav>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav variant='pills' className='me-auto'>
+              <LinkContainer to='/'>
+                <Nav.Link>Jobs</Nav.Link>
+              </LinkContainer>
 
-          {/* hiding search bar until I can add it */}
-          {/*<Form className='d-flex' style={{ marginRight: '8px' }}>
-            <Form.Control
-              type='search'
-              placeholder='Search'
-              className='me-2'
-              aria-label='Search'
-            /> 
-            <Button variant='light'>
-              <CiSearch />
-            </Button>
-          </Form>*/}
+              {admin ? (
+                <LinkContainer to='/testing'>
+                  <Nav.Link>Admin</Nav.Link>
+                </LinkContainer>
+              ) : null}
+            </Nav>
 
-          <ModalProfile />
+            <ModalProfile />
 
-          <Nav>
-            <Button
-              variant='light'
-              onClick={() =>
-                logout({ logoutParams: { returnTo: window.location.origin } })
-              }
-            >
-              Log Out
-            </Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+            <Nav>
+              <Button
+                variant='light'
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                Log Out
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 };
 
 export default NavBar;
