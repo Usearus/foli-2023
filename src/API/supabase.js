c925187090ea20939fead1d3995b640380f18c17
Adam <adamdenais@gmail.com>
2023-04-03 18:57:45 -0500

    Key update

--- src/API/supabase.js
+++ src/API/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseUrl = 'https://rtnxcphjphyiotsorwtx.supabase.co';
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
