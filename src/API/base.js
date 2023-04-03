e508dcb130e610e6e25f7f5cd66d8b290441c8b7
Adam <adamdenais@gmail.com>
2023-04-03 14:27:24 -0500

    Removed unneeded files

--- src/API/base.js
+++
@@ -1,7 +0,0 @@
-import Airtable from 'airtable';
-
-const base = new Airtable({
-  apiKey: process.env.REACT_APP_AIRTABLE_API_KEY,
-}).base(process.env.REACT_APP_AIRTABLE_BASE);
-
-export default base;
