e91941a2d7c80c3fbd40376718083e666a2734f1
Adam <adamdenais@gmail.com>
2023-04-03 19:40:55 -0500

    Added sort to the table header

--- src/Components/JobTable.js
+++ src/Components/JobTable.js
@@ -1,39 +1,153 @@
+import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import JobsTableRow from './JobsTableRow';
 import styled from 'styled-components';
+import { GoChevronDown, GoChevronUp } from 'react-icons/go';
 
 const JobsTable = ({ jobs }) => {
-  return (
-    <Wrapper>
-      <Table responsive hover>
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th>Position</th>
-            <th>Salary</th>
-            <th>Location</th>
-            <th>Status</th>
-            <th>Edited</th>
-            <th></th>
-          </tr>
-        </thead>
+    const [sortOrder, setSortOrder] = useState({
+        column: 'company',
+        direction: 'asc',
+    });
 
-        <tbody>
-          {jobs.map((job) => {
-            return <JobsTableRow key={job.id} {...job} />;
-          })}
-        </tbody>
-      </Table>
-    </Wrapper>
-  );
+    const handleHeaderClick = (column) => {
+        if (column === sortOrder.column) {
+            setSortOrder({
+                column,
+                direction: sortOrder.direction === 'asc' ? 'desc' : 'asc',
+            });
+        } else {
+            setSortOrder({ column, direction: 'asc' });
+        }
+    };
+
+    const sortedJobs = [...jobs].sort((a, b) => {
+        const direction = sortOrder.direction === 'asc' ? 1 : -1;
+        const columnA = a[sortOrder.column];
+        const columnB = b[sortOrder.column];
+
+        if (sortOrder.column === 'salary') {
+            if (columnA === 0 && columnB === 0) {
+                return 0;
+            } else if (columnA === 0) {
+                return 1 * direction;
+            } else if (columnB === 0) {
+                return -1 * direction;
+            } else {
+                return (columnA - columnB) * direction;
+            }
+        } else {
+            return columnA.localeCompare(columnB) * direction;
+        }
+    });
+    console.log(sortedJobs);
+
+    const renderSortIcon = (columnName) => {
+        if (columnName === sortOrder.column) {
+            return sortOrder.direction === 'asc' ? (
+                <GoChevronUp />
+            ) : (
+                <GoChevronDown />
+            );
+        } else {
+            return null;
+        }
+    };
+
+    return (
+        <Wrapper>
+            <Table responsive hover>
+                <thead>
+                    <tr>
+                        <th
+                            className={`custom-width ${
+                                sortOrder.column === 'company'
+                                    ? 'active-header'
+                                    : ''
+                            }`}
+                            onClick={() => handleHeaderClick('company')}
+                        >
+                            Company {renderSortIcon('company')}
+                        </th>
+                        <th
+                            className={`${
+                                sortOrder.column === 'position'
+                                    ? 'active-header'
+                                    : ''
+                            }`}
+                            onClick={() => handleHeaderClick('position')}
+                        >
+                            Position {renderSortIcon('position')}
+                        </th>
+                        <th
+                            className={`${
+                                sortOrder.column === 'salary'
+                                    ? 'active-header'
+                                    : ''
+                            }`}
+                            // TODO onClick={() => handleHeaderClick('salary')}
+                        >
+                            Salary {renderSortIcon('salary')}
+                        </th>
+                        <th
+                            className={`${
+                                sortOrder.column === 'location'
+                                    ? 'active-header'
+                                    : ''
+                            }`}
+                            onClick={() => handleHeaderClick('location')}
+                        >
+                            Location {renderSortIcon('location')}
+                        </th>
+                        <th
+                            className={`${
+                                sortOrder.column === 'status'
+                                    ? 'active-header'
+                                    : ''
+                            }`}
+                            onClick={() => handleHeaderClick('status')}
+                        >
+                            Status{renderSortIcon('status')}
+                        </th>
+                        <th
+                            className={`${
+                                sortOrder.column === 'edited'
+                                    ? 'active-header'
+                                    : ''
+                            }`}
+                            onClick={() => handleHeaderClick('edited')}
+                        >
+                            Edited {renderSortIcon('edited')}
+                        </th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {sortedJobs.map((job) => {
+                        return <JobsTableRow key={job.id} {...job} />;
+                    })}
+                </tbody>
+            </Table>
+        </Wrapper>
+    );
 };
 
 export default JobsTable;
 
 const Wrapper = styled.div`
-  padding: 2rem 2rem;
+    padding: 2rem 2rem;
+    .custom-width {
+        min-width: 120px;
+    }
+    th {
+        cursor: pointer;
+    }
 
-  .table tbody:hover {
-    cursor: pointer;
-  }
+    th.active-header {
+        background-color: var(--grey-100);
+    }
+    .table tbody:hover {
+        cursor: pointer;
+    }
 `;
