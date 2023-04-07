1f29605770b84baa456bc5aa4eea38eb28055bdd
Adam <adamdenais@gmail.com>
2023-04-07 15:48:41 -0500

    Attempting app height to webkit-fill-available

--- src/App.js
+++ src/App.js
@@ -1,64 +1,71 @@
 import { Routes, Route } from 'react-router-dom';
 import {
-  LoginPage,
-  ErrorPage,
-  JobsPage,
-  SingleJobPage,
-  TestingPage,
-  PrivateRoute,
-  AuthWrapper,
+    LoginPage,
+    ErrorPage,
+    JobsPage,
+    SingleJobPage,
+    TestingPage,
+    PrivateRoute,
+    AuthWrapper,
 } from './Pages';
 import NavBar from './Components/NavBar';
 import AlertPopup from './Components/AlertPopup';
+import styled from 'styled-components';
 
 const App = () => {
-  return (
-    <AuthWrapper>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route
-          path='/'
-          element={
-            <PrivateRoute>
-              <AlertPopup />
-              <NavBar />
-              <JobsPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path='/job/:id'
-          element={
-            <PrivateRoute>
-              <AlertPopup />
-              <NavBar />
-              <SingleJobPage />
-            </PrivateRoute>
-          }
-        />
+    return (
+        <Wrapper>
+            <AuthWrapper>
+                <Routes>
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route
+                        path='/'
+                        element={
+                            <PrivateRoute>
+                                <AlertPopup />
+                                <NavBar />
+                                <JobsPage />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path='/job/:id'
+                        element={
+                            <PrivateRoute>
+                                <AlertPopup />
+                                <NavBar />
+                                <SingleJobPage />
+                            </PrivateRoute>
+                        }
+                    />
 
-        <Route
-          path='/testing'
-          element={
-            <PrivateRoute>
-              <AlertPopup />
-              <NavBar />
-              <TestingPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path='*'
-          element={
-            <>
-              <NavBar />
-              <ErrorPage />
-            </>
-          }
-        />
-      </Routes>
-    </AuthWrapper>
-  );
+                    <Route
+                        path='/testing'
+                        element={
+                            <PrivateRoute>
+                                <AlertPopup />
+                                <NavBar />
+                                <TestingPage />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path='*'
+                        element={
+                            <>
+                                <NavBar />
+                                <ErrorPage />
+                            </>
+                        }
+                    />
+                </Routes>
+            </AuthWrapper>
+        </Wrapper>
+    );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+    height: -webkit-fill-available;
+`;
