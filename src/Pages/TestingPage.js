a2f92f2f3f4ad749f4f1a6b0ecf70b6d693aa843
Adam <adamdenais@gmail.com>
2023-05-06 10:53:37 -0500

    Testing out jsPDF

--- src/Pages/TestingPage.js
+++ src/Pages/TestingPage.js
@@ -1,13 +1,43 @@
-const TestingPage = () => {
-    
+import { jsPDF } from 'jspdf';
+import { Button } from 'react-bootstrap';
+import { DatabaseContext } from '../context/DatabaseContext';
+import { useContext } from 'react';
 
-    return (
-        <>
-            <h3>Testing Page</h3>
-            <hr />
-            
-        </>
-    );
+const TestingPage = () => {
+	const { allPages } = useContext(DatabaseContext);
+	const myPage = allPages[6];
+
+	if (myPage) {
+		const content = myPage.content;
+		// console.log('myPage', content);
+
+		const handleExportPdf = () => {
+			const doc = new jsPDF('p', 'px', 'letter');
+			// const doc = new jsPDF();
+
+			const options = {
+				callback: function (doc) {
+					doc.save(`page.pdf`);
+				},
+				// html2canvas: { scale: 0.3 },
+				width: 400,
+				windowWidth: 400,
+				hotfixes: ['px_scaling'],
+				margin: [10, 10, 10, 10],
+			};
+
+			doc.html(content, options);
+		};
+
+		return (
+			<>
+				<h3>Testing Page</h3>
+				<hr />
+				<p>{myPage.content}</p>
+				<Button onClick={handleExportPdf}>Export PDF</Button>
+			</>
+		);
+	}
 };
 
 export default TestingPage;
