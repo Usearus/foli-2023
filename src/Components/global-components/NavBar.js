73b721c5082ed8a42c64dc412db859bc900f3535
Adam <adamdenais@gmail.com>
2023-05-14 09:55:17 -0500

    Hiding theme button in nav bar

--- src/Components/global-components/NavBar.js
+++ src/Components/global-components/NavBar.js
@@ -13,48 +13,48 @@ import ModalProfile from '../modal-components/ModalProfile';
 import { DatabaseContext } from '../../context/DatabaseContext';
 import { FiMenu } from 'react-icons/fi';
 import SiteIcon from '../atom-components/SiteIcon';
-import useAlert from '../../Custom Hooks/useAlert';
-import { supabase } from '../../API/supabase';
+// import useAlert from '../../Custom Hooks/useAlert';
+// import { supabase } from '../../API/supabase';
 
 const NavBar = () => {
 	const { userProfile, adminProfile, fetchUserProfile } =
 		useContext(DatabaseContext);
-	const { setAlert } = useAlert();
+	// const { setAlert } = useAlert();
 
-	const handleSaveThemeClick = async () => {
-		if (userProfile.theme === 'light') {
-			const { error } = await supabase
-				.from('profiles')
-				.update({
-					theme: 'dark',
-				})
-				.eq('id', userProfile.id);
-			fetchUserProfile();
-			setAlert('Theme updated.', 'success');
+	// const handleSaveThemeClick = async () => {
+	// 	if (userProfile.theme === 'light') {
+	// 		const { error } = await supabase
+	// 			.from('profiles')
+	// 			.update({
+	// 				theme: 'dark',
+	// 			})
+	// 			.eq('id', userProfile.id);
+	// 		fetchUserProfile();
+	// 		setAlert('Theme updated.', 'success');
 
-			if (error) {
-				setAlert('Something went wrong. Preferences not updated.', 'danger');
-				console.log('error is', error);
-				return;
-			}
-		}
-		if (userProfile.theme === 'dark') {
-			const { error } = await supabase
-				.from('profiles')
-				.update({
-					theme: 'light',
-				})
-				.eq('id', userProfile.id);
-			fetchUserProfile();
-			setAlert('Theme updated.', 'success');
+	// 		if (error) {
+	// 			setAlert('Something went wrong. Preferences not updated.', 'danger');
+	// 			console.log('error is', error);
+	// 			return;
+	// 		}
+	// 	}
+	// 	if (userProfile.theme === 'dark') {
+	// 		const { error } = await supabase
+	// 			.from('profiles')
+	// 			.update({
+	// 				theme: 'light',
+	// 			})
+	// 			.eq('id', userProfile.id);
+	// 		fetchUserProfile();
+	// 		setAlert('Theme updated.', 'success');
 
-			if (error) {
-				setAlert('Something went wrong. Preferences not updated.', 'danger');
-				console.log('error is', error);
-				return;
-			}
-		}
-	};
+	// 		if (error) {
+	// 			setAlert('Something went wrong. Preferences not updated.', 'danger');
+	// 			console.log('error is', error);
+	// 			return;
+	// 		}
+	// 	}
+	// };
 
 	if (userProfile) {
 		return (
@@ -113,11 +113,11 @@ const NavBar = () => {
 									</>
 								) : null}
 							</Nav>
-							<Button
+							{/* <Button
 								style={{ marginRight: '1rem' }}
 								onClick={handleSaveThemeClick}>
 								{userProfile.theme}
-							</Button>
+							</Button> */}
 							<ModalProfile />
 						</Offcanvas.Body>
 					</Navbar.Offcanvas>
