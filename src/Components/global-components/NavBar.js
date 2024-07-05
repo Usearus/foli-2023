79235e87a7930a2bbf8e11923f48dd84cb992e5a
Adam <adamdenais@gmail.com>
2024-07-05 13:48:12 -0500

    Unsure what this is

--- src/Components/global-components/NavBar.js
+++ src/Components/global-components/NavBar.js
@@ -3,10 +3,11 @@ import Container from 'react-bootstrap/Container';
 import {
 	Nav,
 	Navbar,
-	Button,
+	// Button,
 	Offcanvas,
 	OffcanvasHeader,
 	OffcanvasTitle,
+	Stack,
 } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import ModalProfile from '../modal-components/ModalProfile';
@@ -15,6 +16,7 @@ import { FiMenu } from 'react-icons/fi';
 import SiteIcon from '../atom-components/SiteIcon';
 // import useAlert from '../../Custom Hooks/useAlert';
 // import { supabase } from '../../API/supabase';
+import { IconButton, Button } from '@fluentui/react-components';
 
 const NavBar = () => {
 	const { userProfile, adminProfile } = useContext(DatabaseContext);
@@ -36,6 +38,7 @@ const NavBar = () => {
 					top: '0',
 					zIndex: '1',
 				}}>
+				{/* Mobile NOT OPENED*/}
 				<Container fluid>
 					<LinkContainer to='/'>
 						<Nav.Link active={false}>
@@ -50,6 +53,8 @@ const NavBar = () => {
 							<FiMenu />
 						</Button>
 					</Navbar.Toggle>
+
+					{/* Mobile OPENED*/}
 					<Navbar.Offcanvas
 						id='offcanvas-navbar-nav'
 						placement='start'
@@ -57,29 +62,34 @@ const NavBar = () => {
 							background: 'var(--grey-100)',
 							cursor: 'pointer',
 						}}>
+						{/* Navigation Header on sidebar modal */}
 						<OffcanvasHeader closeButton>
 							<OffcanvasTitle id='title'>
 								<SiteIcon />
 							</OffcanvasTitle>
 						</OffcanvasHeader>
+
+						{/* Navigation Header on sidebar modal */}
 						<Offcanvas.Body>
-							<Nav variant='pills' className='me-auto'>
+							<Stack>
 								<LinkContainer to='/'>
-									<Nav.Link active={false}>Jobs</Nav.Link>
+									<Button active={false}>Jobs</Button>
 								</LinkContainer>
 								{adminProfile ? (
 									<>
 										<LinkContainer to='/resume'>
-											<Nav.Link active={false}>Resume</Nav.Link>
+											<Button active={false}>Resume</Button>
 										</LinkContainer>
 										<LinkContainer to='/testing'>
 											<Nav.Link active={false}>Admin</Nav.Link>
 										</LinkContainer>
 									</>
 								) : null}
-							</Nav>
+							</Stack>
+
 							{/* Show this if you want to test the auto-increment function 
 							{IncrementNumberFromDatabase()}*/}
+
 							<ModalProfile />
 						</Offcanvas.Body>
 					</Navbar.Offcanvas>
