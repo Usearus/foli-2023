05c444b283e2a86491d4f32eedfd73b559939cf9
Adam <adamdenais@gmail.com>
2024-08-30 14:44:18 -0500

    Added mobile variant of page component with snap styling

--- src/Components/single-job-components/Page.js
+++ src/Components/single-job-components/Page.js
@@ -134,286 +134,208 @@ const Page = (page) => {
 		return <></>;
 	}
 
-	// MOBILE PAGE
-	// if (isMobile === true && initialVisibleValue === true) {
-	// 	return (
-	// 		<>
-	// 			<article className='bg-base-200 w-[92vw] h-hull py-4 px-0 flex flex-col gap-2'>
-	// 				{/* Title Content */}
-	// 				<header>
-	// 					{editing ? (
-	// 						<div className='flex justify-between'>
-	// 							{page.locked ? (
-	// 								<label className='font-bold pl-2'>{page.title}</label>
-	// 							) : (
-	// 								<label className='input input-xs flex grow items-center gap-2 mb-[2px]'>
-	// 									<input
-	// 										type='text'
-	// 										required
-	// 										ref={titleRef}
-	// 										defaultValue={initialTitleValue}
-	// 										maxLength={titleMaxChar}
-	// 										onChange={handleTitleChange}
-	// 										placeholder='Add page title'
-	// 										className='grow font-bold text-base'
-	// 									/>
-	// 									<span className='label-text-alt'>
-	// 										{characterCount}/{titleMaxChar}
-	// 									</span>
-	// 								</label>
-	// 							)}
-	// 						</div>
-	// 					) : (
-	// 						<div className='flex justify-between'>
-	// 							<label className='font-bold pl-[9px]'>{page.title}</label>
-	// 							<div className='flex justify-end gap-2'>
-	// 								{page.locked ? (
-	// 									''
-	// 								) : (
-	// 									<div className='dropdown dropdown-end'>
-	// 										<div
-	// 											tabIndex={0}
-	// 											role='button'
-	// 											className='btn btn-xs btn-ghost '>
-	// 											<DotsVerticalIcon />
-	// 										</div>
-	// 										<ul
-	// 											tabIndex={0}
-	// 											className='dropdown-content menu bg-base-300 rounded-box z-[1] w-52 p-2 shadow'>
-	// 											{/* Delete option */}
-	// 											<li>
-	// 												<button
-	// 													className='text-error'
-	// 													onClick={() =>
-	// 														document
-	// 															.getElementById('delete_page_modal')
-	// 															.showModal()
-	// 													}>
-	// 													Delete
-	// 												</button>
-	// 											</li>
-	// 										</ul>
-	// 									</div>
-	// 								)}
-	// 							</div>
-	// 						</div>
-	// 					)}
-	// 				</header>
-	// 				<hr />
-
-	// 				{/* Markdown/Quill content */}
-	// 				{editing ? (
-	// 					// Quill version
-	// 					<div className='flex flex-col justify-between h-full'>
-	// 						<div className='page-scroll'>
-	// 							<ReactQuillEditor
-	// 								value={content}
-	// 								onChange={handleEditorChange}
-	// 							/>
-	// 						</div>
-	// 						<div className='flex justify-end gap-2'>
-	// 							<button
-	// 								type='button'
-	// 								className='btn btn-sm btn-ghost btn-primary w-fit'
-	// 								onClick={handleCancelClick}>
-	// 								Cancel
-	// 							</button>
-	// 							<button
-	// 								type='button'
-	// 								className='btn btn-sm btn-primary w-fit'
-	// 								onClick={handleUpdateContentClick}>
-	// 								Save page
-	// 							</button>
-	// 						</div>
-	// 					</div>
-	// 				) : (
-	// 					// Markdown version
-	// 					<div className='flex flex-col justify-between h-full'>
-	// 						<MarkdownView
-	// 							className='page-scroll markdown-content'
-	// 							markdown={page.content}
-	// 						/>
-	// 						<div className='flex justify-end'>
-	// 							<button
-	// 								type='button'
-	// 								className='btn btn-sm btn-outline btn-primary w-fit'
-	// 								onClick={handleEditClick}>
-	// 								Edit page
-	// 							</button>
-	// 						</div>
-	// 					</div>
-	// 				)}
-	// 			</article>
-	// 		</>
-	// 	);
-	// }
-
-	// 	// DESKTOP PAGE
-	// if (isMobile === false) {
 	return (
 		<>
-			<Resizable
-				enable={{
-					top: false,
-					right: true,
-					bottom: false,
-					left: false,
-					topRight: false,
-					bottomRight: false,
-					bottomLeft: false,
-					topLeft: false,
-				}}
-				onResizeStop={(e, direction, ref, d) => {
-					const newPageWidth = pageWidth + d.width;
-					handleUpdateWidthClick(newPageWidth);
-				}}
-				minWidth='300px'
-				maxWidth='700px'
-				size={{ height: '100%', width: pageWidth }}
-				className='h-full p-4 flex flex-col gap-2 bg-base-100'>
-				{/* Title Content */}
-				<header>
-					{editing ? (
-						<div className='flex justify-between'>
-							{page.locked ? (
-								<label className='font-bold pl-2'>{page.title}</label>
-							) : (
-								<label className='input input-ghost input-xs flex grow items-center gap-2 mb-[2px]'>
-									<input
-										type='text'
-										required
-										ref={titleRef}
-										defaultValue={initialTitleValue}
-										maxLength={titleMaxChar}
-										onChange={handleTitleChange}
-										placeholder='Add page title'
-										className='grow font-bold text-base '
-									/>
-
-									<span className='label-text-alt'>
-										{characterCount}/{titleMaxChar}
-									</span>
-								</label>
-							)}
-						</div>
-					) : (
-						<div className='flex justify-between'>
-							<label className='font-bold pl-[9px]'>{page.title}</label>
-							<div className='flex justify-end gap-2'>
-								<button
-									className='btn btn-xs btn-ghost'
-									onClick={handleEditClick}>
-									<Pencil1Icon />
-								</button>
+			{isMobile === false && initialVisibleValue === true ? (
+				// Desktop Version
+				<Resizable
+					enable={{
+						top: false,
+						right: true,
+						bottom: false,
+						left: false,
+						topRight: false,
+						bottomRight: false,
+						bottomLeft: false,
+						topLeft: false,
+					}}
+					onResizeStop={(e, direction, ref, d) => {
+						const newPageWidth = pageWidth + d.width;
+						handleUpdateWidthClick(newPageWidth);
+					}}
+					minWidth='300px'
+					maxWidth='700px'
+					size={{ height: '100%', width: pageWidth }}
+					className='h-full p-4 flex flex-col gap-2 bg-base-100'>
+					{/* Title Content */}
+					<header>
+						{editing ? (
+							<div className='flex justify-between'>
 								{page.locked ? (
-									''
+									<label className='font-bold pl-2'>{page.title}</label>
 								) : (
-									<div className='dropdown dropdown-end'>
-										<button tabIndex={0} className='btn btn-xs btn-ghost '>
-											<DotsVerticalIcon />
-										</button>
-										<ul
-											tabIndex={0}
-											className='dropdown-content menu bg-base-200 rounded-box z-[1] w-52 p-2 shadow'>
-											<li>
-												<DeletePageBtn page={page} />
-											</li>
-										</ul>
-									</div>
+									<label className='input input-ghost input-xs flex grow items-center gap-2 mb-[2px]'>
+										<input
+											type='text'
+											required
+											ref={titleRef}
+											defaultValue={initialTitleValue}
+											maxLength={titleMaxChar}
+											onChange={handleTitleChange}
+											placeholder='Add page title'
+											className='grow font-bold text-base'
+										/>
+
+										<span className='label-text-alt'>
+											{characterCount}/{titleMaxChar}
+										</span>
+									</label>
 								)}
 							</div>
+						) : (
+							<div className='flex justify-between'>
+								<label className='font-bold pl-[9px]'>{page.title}</label>
+								<div className='flex justify-end gap-2'>
+									<button
+										className='btn btn-xs btn-ghost'
+										onClick={handleEditClick}>
+										<Pencil1Icon />
+									</button>
+									{!page.locked && (
+										<div className='dropdown dropdown-end'>
+											<button tabIndex={0} className='btn btn-xs btn-ghost'>
+												<DotsVerticalIcon />
+											</button>
+											<ul
+												tabIndex={0}
+												className='dropdown-content menu bg-base-200 rounded-box z-[1] w-52 p-2 shadow'>
+												<li>
+													<DeletePageBtn page={page} />
+												</li>
+											</ul>
+										</div>
+									)}
+								</div>
+							</div>
+						)}
+					</header>
+					<div className='divider m-0' />
+
+					{/* Markdown/Quill content */}
+					{editing ? (
+						<div className='flex flex-col justify-between h-full'>
+							<div className='page-scroll'>
+								<ReactQuillEditor
+									value={content}
+									onChange={handleEditorChange}
+								/>
+							</div>
+							<div className='flex justify-end gap-2'>
+								<button
+									type='button'
+									className='btn btn-sm btn-ghost btn-primary w-fit'
+									onClick={handleCancelClick}>
+									Cancel
+								</button>
+								<button
+									type='button'
+									className='btn btn-sm btn-primary w-fit'
+									onClick={handleUpdateContentClick}>
+									Save page
+								</button>
+							</div>
+						</div>
+					) : (
+						<div className='flex flex-col justify-between h-full'>
+							<MarkdownView
+								className='page-scroll markdown-content'
+								markdown={page.content}
+							/>
 						</div>
 					)}
-				</header>
-				<div className='divider m-0' />
+				</Resizable>
+			) : (
+				// Mobile Version
+				<div className='h-full w-[90vw] p-4 flex flex-col gap-2 bg-base-100'>
+					{/* Title Content */}
+					<header>
+						{editing ? (
+							<div className='flex justify-between'>
+								{page.locked ? (
+									<label className='font-bold pl-2'>{page.title}</label>
+								) : (
+									<label className='input input-ghost input-xs flex grow items-center gap-2 mb-[2px]'>
+										<input
+											type='text'
+											required
+											ref={titleRef}
+											defaultValue={initialTitleValue}
+											maxLength={titleMaxChar}
+											onChange={handleTitleChange}
+											placeholder='Add page title'
+											className='grow font-bold text-base'
+										/>
+										<span className='label-text-alt'>
+											{characterCount}/{titleMaxChar}
+										</span>
+									</label>
+								)}
+							</div>
+						) : (
+							<div className='flex justify-between'>
+								<label className='font-bold pl-[9px]'>{page.title}</label>
+								<div className='flex justify-end gap-2'>
+									<button
+										className='btn btn-xs btn-ghost'
+										onClick={handleEditClick}>
+										<Pencil1Icon />
+									</button>
+									{!page.locked && (
+										<div className='dropdown dropdown-end'>
+											<button tabIndex={0} className='btn btn-xs btn-ghost'>
+												<DotsVerticalIcon />
+											</button>
+											<ul
+												tabIndex={0}
+												className='dropdown-content menu bg-base-200 rounded-box z-[1] w-52 p-2 shadow'>
+												<li>
+													<DeletePageBtn page={page} />
+												</li>
+											</ul>
+										</div>
+									)}
+								</div>
+							</div>
+						)}
+					</header>
+					<div className='divider m-0' />
 
-				{/* Markdown/Quill content */}
-				{editing ? (
-					// Quill version
-					<div className='flex flex-col justify-between h-full'>
-						<div className='page-scroll'>
-							<ReactQuillEditor value={content} onChange={handleEditorChange} />
+					{/* Markdown/Quill content */}
+					{editing ? (
+						<div className='flex flex-col justify-between h-full'>
+							<div className='page-scroll'>
+								<ReactQuillEditor
+									value={content}
+									onChange={handleEditorChange}
+								/>
+							</div>
+							<div className='flex justify-end gap-2'>
+								<button
+									type='button'
+									className='btn btn-sm btn-ghost btn-primary w-fit'
+									onClick={handleCancelClick}>
+									Cancel
+								</button>
+								<button
+									type='button'
+									className='btn btn-sm btn-primary w-fit'
+									onClick={handleUpdateContentClick}>
+									Save page
+								</button>
+							</div>
 						</div>
-						<div className='flex justify-end gap-2'>
-							<button
-								type='button'
-								className='btn btn-sm btn-ghost btn-primary w-fit'
-								onClick={handleCancelClick}>
-								Cancel
-							</button>
-							<button
-								type='button'
-								className='btn btn-sm btn-primary w-fit'
-								onClick={handleUpdateContentClick}>
-								Save page
-							</button>
+					) : (
+						<div className='flex flex-col justify-between h-full'>
+							<MarkdownView
+								className='page-scroll markdown-content'
+								markdown={page.content}
+							/>
 						</div>
-					</div>
-				) : (
-					// Markdown version
-					<div className='flex flex-col justify-between h-full'>
-						<MarkdownView
-							className='page-scroll markdown-content'
-							markdown={page.content}
-						/>
-					</div>
-				)}
-			</Resizable>
+					)}
+				</div>
+			)}
 		</>
 	);
 };
-// };
-export default Page;
 
-// 	// MOBILE PAGE
-// 	if (isMobile === true && initialVisibleValue === true) {
-// 		return (
-// 			<Wrapper>
-// 				<div className='page-content pages-mobile'>
-// 					<header className='page-title'>
-// 						{/* {!editing ? ( */}
-// 						<Stack direction='horizontal'>
-// 							<h6 style={{ color: 'var(--primary-700)' }}>{page.title}</h6>
-// 							<Stack direction='horizontal' className='ms-auto'>
-// 								<Button
-// 									variant='light'
-// 									style={{
-// 										background: 'var(--white)',
-// 										border: 0,
-// 									}}
-// 									onClick={handleOpenPageModalClick}>
-// 									<AiFillEdit />
-// 								</Button>
-// 								{page.locked ? (
-// 									''
-// 								) : (
-// 									<Dropdown onSelect={handleSelect}>
-// 										<Dropdown.Toggle
-// 											id='dropdown'
-// 											variant='link'
-// 											style={{
-// 												color: 'var(--grey-800)',
-// 											}}>
-// 											<FiMoreVertical />
-// 										</Dropdown.Toggle>
-// 										<Dropdown.Menu>
-// 											<Dropdown.Item eventKey='1'>Delete page</Dropdown.Item>
-// 										</Dropdown.Menu>
-// 									</Dropdown>
-// 								)}
-// 							</Stack>
-// 							{showEditPageModal && (
-// 								<ModalEditPage
-// 									showEditPageModal={showEditPageModal}
-// 									handleCancelClick={handleCancelClick}
-// 									page={page}
-// 									content={content}
-// 									setContent={setContent}
-// 									initialTitleValue={initialTitleValue}
-// 									titleRef={titleRef}
-// 									titleMaxChar={titleMaxChar}
-// 									handleTitleChange={handleTitleChange}
-// 									characterCount={characterCount}
-// 									handleUpdateContentClick={handleUpdateContentClick}
-// 									handleEditorChange={handleEditorChange}
-// 								/>
+export default Page;
--- src/Components/single-job-components/PageList.js
+++ src/Components/single-job-components/PageList.js
@@ -39,7 +39,7 @@ const PageList = () => {
 
 	if (visiblePages.length > 0) {
 		return (
-			<div className='flex gap-4 h-full overflow-x-auto'>
+			<div className='flex gap-4 h-full overflow-x-auto snap-x snap-mandatory lg:snap-none'>
 				{visiblePages.map((page, index) => {
 					const ref = createRef();
 					pageRef.current[index] = { id: page.id, ref };
@@ -49,8 +49,7 @@ const PageList = () => {
 					return (
 						<div
 							key={page.id}
-							style={{ scrollSnapAlign: 'center' }}
-							className={`h-full ${pageClassName}`}
+							className={`h-full snap-center lg:snap-align-none ${pageClassName}`}
 							ref={ref}>
 							<Page key={page.id} id={page.id} {...page} />
 						</div>
@@ -71,83 +70,3 @@ const PageList = () => {
 };
 
 export default PageList;
-
-// const PageList = ({ className }) => {
-// 	const { currentPages, settingPageStack, selectedPageID } =
-// 		useContext(DatabaseContext);
-// 	const visiblePages = currentPages.filter((page) => page.visible);
-
-// 	const stackClassName =
-// 		settingPageStack === 'horizontal'
-// 			? 'horizontal-stack-page'
-// 			: settingPageStack === 'vertical'
-// 			? 'vertical-stack-page'
-// 			: '';
-
-// 	// create a ref to the Pages
-// 	const pageRef = useRef([]);
-// 	const lastPageRef = useRef(null);
-// 	const prevLengthRef = useRef(currentPages.length);
-
-// 	useEffect(() => {
-// 		const pageToScrollTo = pageRef.current.find(
-// 			(page) => page.id === selectedPageID
-// 		);
-// 		// Scroll to the Page that is selected in Sidebar
-// 		if (pageToScrollTo && pageToScrollTo.ref.current) {
-// 			pageToScrollTo.ref.current.scrollIntoView({ behavior: 'smooth' });
-// 		} else {
-// 			// If scrollIntoView is null or pageToScrollTo is not found, set the current page to the first page
-// 			const firstPage = pageRef.current[0];
-// 			if (firstPage && firstPage.ref.current) {
-// 				firstPage.ref.current.scrollIntoView({ behavior: 'smooth' });
-// 			}
-// 		}
-
-// 		// Scroll to the last page when currentPages increases
-// 		if (currentPages.length > prevLengthRef.current) {
-// 			if (lastPageRef.current && lastPageRef.current.scrollIntoView) {
-// 				lastPageRef.current.scrollIntoView({ behavior: 'smooth' });
-// 			}
-// 		}
-
-// 		// Update prevLengthRef.current
-// 		prevLengthRef.current = currentPages.length;
-// 	}, [selectedPageID, currentPages.length]);
-
-// 	if (visiblePages.length > 0) {
-// 		return (
-// 			<Wrapper className={`${className}`}>
-// 				{visiblePages.map((page, index) => {
-// 					const ref = createRef();
-// 					pageRef.current[index] = { id: page.id, ref };
-// 					const isSelectedPage = page.id === selectedPageID;
-// 					const pageClassName = isSelectedPage ? 'selected-page' : '';
-
-// 					return (
-// 						<div
-// 							key={page.id}
-// 							style={{ scrollSnapAlign: 'center' }}
-// 							className={`${stackClassName} ${pageClassName}`}
-// 							ref={ref}>
-// 							<Page key={page.id} id={page.id} {...page} />
-// 						</div>
-// 					);
-// 				})}
-// 				<div ref={lastPageRef} style={{ visibility: 'hidden' }} />
-// 			</Wrapper>
-// 		);
-// 	}
-
-// 	if (visiblePages.length === 0) {
-// 		return (
-// 			<Wrapper className={`${className}`}>
-// 				<div className='empty-state'>
-// 					<h5>No pages are visible. Toggle visibility in Pages list.</h5>
-// 				</div>
-// 			</Wrapper>
-// 		);
-// 	}
-// };
-
-// export default PageList;
